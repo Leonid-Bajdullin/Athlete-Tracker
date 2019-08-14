50201ede9227b0d98afa9357f1430c7d411ff246
unknown <lbajdull@gmail.com>
2019-08-14 19:48:46 +0300

    modify some methods to work properly

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -6,20 +6,22 @@ import {
     OnUndefined,
     Param,
     Post,
-    Put,
-    Req
+    Req,
+    UseBefore
 } from 'routing-controllers';
 
 import { UserNotFoundError } from '../errors/UserNotFoundError';
 import { User } from '../models/User';
 import { UserService } from '../services/UserService';
 import { UserRequestDto } from '../dto/UserRequestDto';
+import { CheckAuthMiddleware } from '../middlewares/CheckAuthMiddlware';
 
 // @Authorized()
 @JsonController('/users')
 export class UserController {
     constructor(private userService: UserService) {}
 
+    @UseBefore(CheckAuthMiddleware)
     @Get()
     public find(): Promise<User[]> {
         return this.userService.find();
@@ -41,16 +43,16 @@ export class UserController {
         return this.userService.create(user);
     }
 
-    @Put('/:id')
-    public update(
-        @Param('id') id: string,
-        @Body() user: UserRequestDto
-    ): Promise<User> {
-        return this.userService.update(id, user);
-    }
+    // @Put('/:id')
+    // public update(
+    //     @Param('id') id: string,
+    //     @Body() user: UserRequestDto
+    // ): Promise<User> {
+    //     return this.userService.update(id, user);
+    // }
 
     @Delete('/:id')
-    public delete(@Param('id') id: string): Promise<string> {
+    public delete(@Param('id') id: string): Promise<{}> {
         return this.userService.delete(id);
     }
 }
