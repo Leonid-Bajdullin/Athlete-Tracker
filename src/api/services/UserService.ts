bf13ed66f0440e556d138e210d7ade5f8364a107
unknown <lbajdull@gmail.com>
2019-08-14 19:50:30 +0300

    modify some methods to work properly

--- src/api/services/UserService.ts
+++ src/api/services/UserService.ts
@@ -1,7 +1,11 @@
 import { Service } from 'typedi';
 import { OrmRepository } from 'typeorm-typedi-extensions';
+import * as bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+// import passport from 'passport';
 // import uuid from 'uuid';
 
+import { env } from '../../env';
 import {
     EventDispatcher,
     EventDispatcherInterface
@@ -9,45 +13,104 @@ import {
 import { Logger, LoggerInterface } from '../../decorators/Logger';
 import { User } from '../models/User';
 import { UserRepository } from '../repositories/UserRepository';
+import { AccountRepository } from '../repositories/AccountRepository';
 import { events } from '../subscribers/events';
 import { UserRequestDto } from '../dto/UserRequestDto';
+import { Account } from '../models/Account';
+import { LoginDto } from '../dto/LoginDto';
 
 @Service()
 export class UserService {
     constructor(
         @OrmRepository() private userRepository: UserRepository,
+        @OrmRepository() private accountRepository: AccountRepository,
         @EventDispatcher() private eventDispatcher: EventDispatcherInterface,
         @Logger(__filename) private log: LoggerInterface
     ) {}
 
+    public hashPassword(password: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            bcrypt.hash(password, 10, (err, hash) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(hash);
+            });
+        });
+    }
+
+    public comparePassword(
+        account: Account,
+        password: string
+    ): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, account.salt, (err, res) => {
+                resolve(res === true);
+            });
+        });
+    }
+
     public find(): Promise<User[]> {
         this.log.info('Find all users');
-        return this.userRepository.find({ relations: ['userTeams'] });
+        return this.userRepository.find({
+            relations: ['userTeams', 'accounts']
+        });
     }
 
     public findOne(id: string): Promise<User | undefined> {
         this.log.info('Find one user');
-        return this.userRepository.findOne({ id });
+        return this.userRepository.findOne({
+            where: { id: id },
+            relations: ['userTeams', 'accounts']
+        });
     }
 
     public async create(user: UserRequestDto): Promise<User> {
-        this.log.info('Create a new user => ', user);
-        // user.id = uuid.v1();
         const newUser = await this.userRepository.save(user);
-        this.eventDispatcher.dispatch(events.user.created, newUser);
-        return newUser;
+        const returnedUser = await this.findOne(newUser.id);
+
+        this.eventDispatcher.dispatch(events.user.created, returnedUser);
+
+        const account = new Account();
+        account.salt = await this.hashPassword(user.password);
+        account.user = returnedUser;
+        account.provider = 'login';
+        await this.accountRepository.save(account);
+        return returnedUser;
     }
 
-    public update(id: string, user: UserRequestDto): Promise<User> {
-        this.log.info('Update a user');
-        user.id = id;
-        return this.userRepository.save(user);
-    }
+    // public update(id: string, user: UserRequestDto): Promise<User> {
+    //     this.log.info('Update a user');
+    //     user.id = id;
+    //     return this.userRepository.save(user);
+    // }
 
-    public async delete(id: string): Promise<string> {
+    public async delete(id: string): Promise<{}> {
         this.log.info('Delete a user');
         await this.userRepository.delete(id);
-        const deleteMessage = `User with userID=${id} deleted successfully!`;
+        const deleteMessage = {
+            message: `User with userID=${id} deleted successfully!`
+        };
         return deleteMessage;
+    }
+
+    public async login(loginData: LoginDto): Promise<{}> {
+        const user = await this.userRepository.findOne({
+            where: { email: loginData.email },
+            relations: ['accounts']
+        });
+        const account = user.accounts.find((item) => item.provider === 'login');
+        this.log.info('account =>', account);
+        if (this.comparePassword(account, loginData.password)) {
+            const token = jwt.sign({ id: account.id }, env.jwt.jwt_secret);
+            return {
+                token: token
+            };
+        }
+        this.log.info(
+            'compared password correct => ',
+            this.comparePassword(account, loginData.password)
+        );
+        return { message: 'Password incorrect' };
     }
 }
