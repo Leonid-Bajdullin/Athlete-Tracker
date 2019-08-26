311dbecd07c966ec61a9ecfcc9e1b9089bcc85e8
Leo <lbajdull@gmail.com>
2019-08-27 01:22:10 +0300

    split dto structure separately for each request

---
+++ src/api/dto/user/UserCreateDto.ts
@@ -0,0 +1,7 @@
+export class UserCreateDto {
+    public firstName: string;
+    public lastName: string;
+    public email: string;
+    public password: string;
+    // public passwordConfirmation: string;
+}
---
+++ src/api/dto/user/UserSubmitChangesDto.ts
@@ -0,0 +1,10 @@
+export class UserSubmitChangesDto {
+    public firstName: string;
+    public lastName: string;
+    public email: string;
+    public password: string;
+    public data: object;
+    public nickname: string;
+    public phone: string;
+    public photoUrl: string;
+}
