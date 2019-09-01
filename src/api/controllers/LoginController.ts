65b09009767374d3f63eb34a107368e3cb104ac7
Leo <lbajdull@gmail.com>
2019-09-01 23:38:22 +0300

    add an exception for unsuccessful login

--- src/api/controllers/LoginController.ts
+++ src/api/controllers/LoginController.ts
@@ -1,13 +1,15 @@
-import { JsonController, Body, Post, UseAfter } from 'routing-controllers';
-import { CheckAuthMiddleware } from '../middlewares/CheckAuthMiddlware';
+import { JsonController, Body, Post, OnUndefined } from 'routing-controllers';
+// import { CheckAuthMiddleware } from '../middlewares/CheckAuthMiddlware';
 import { UserService } from '../services/UserService';
 import { LoginDto } from '../dto/user/LoginDto';
+import { UserNotFoundError } from '../errors/UserNotFoundError';
 
 @JsonController('/login')
 export class LoginController {
     constructor(private userService: UserService) {}
 
-    @UseAfter(CheckAuthMiddleware)
+    // @UseAfter(CheckAuthMiddleware)
+    @OnUndefined(UserNotFoundError)
     @Post()
     public login(@Body() loginData: LoginDto): Promise<{}> {
         return this.userService.login(loginData);
