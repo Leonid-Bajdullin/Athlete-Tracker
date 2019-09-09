cce94ee01910c8f2cb7663f3ae51771b49fa4849
Leo <lbajdull@gmail.com>
2019-09-09 08:56:26 +0300

    change DTO to use constructor

--- src/api/dto/user/UserProfileChangesDto.ts
+++ src/api/dto/user/UserProfileChangesDto.ts
@@ -1,9 +1,21 @@
+import { User } from 'src/api/models/User';
+
 export class UserProfileChangesDto {
+    constructor(user: User) {
+        this.id = user.id;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.email = user.email;
+        this.data = user.data;
+        this.nickname = user.nickname;
+        this.phone = user.phone;
+        this.photoUrl = user.photoUrl;
+    }
+    public id: string;
     public firstName: string;
     public lastName: string;
     public email: string;
-    public password: string;
-    public data: object;
+    public data: JSON;
     public nickname: string;
     public phone: string;
     public photoUrl: string;
