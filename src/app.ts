0b3277075c364ef6e0831326a92a4123748b490b
unknown <lbajdull@gmail.com>
2019-09-09 15:44:16 +0300

    comment unnecessary loaders

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,17 @@ import { bootstrapMicroframework } from 'microframework-w3tec';
 
 import { banner } from './lib/banner';
 import { Logger } from './lib/logger';
-import { eventDispatchLoader } from './loaders/eventDispatchLoader';
-import { expressLoader } from './loaders/expressLoader';
+// import { eventDispatchLoader } from './loaders/eventDispatchLoader';
 // import { graphqlLoader } from './loaders/graphqlLoader';
-import { homeLoader } from './loaders/homeLoader';
-import { iocLoader } from './loaders/iocLoader';
-import { monitorLoader } from './loaders/monitorLoader';
+// import { monitorLoader } from './loaders/monitorLoader';
+// import { swaggerLoader } from './loaders/swaggerLoader';
+// import { homeLoader } from './loaders/homeLoader';
 import { publicLoader } from './loaders/publicLoader';
-import { swaggerLoader } from './loaders/swaggerLoader';
-import { typeormLoader } from './loaders/typeormLoader';
 import { winstonLoader } from './loaders/winstonLoader';
+
+import { expressLoader } from './loaders/expressLoader';
+import { typeormLoader } from './loaders/typeormLoader';
+import { iocLoader } from './loaders/iocLoader';
 
 /**
  * EXPRESS TYPESCRIPT BOILERPLATE
@@ -31,15 +32,18 @@ bootstrapMicroframework({
      * bootstrap process. All loaders are executed one by one in a sequential order.
      */
     loaders: [
-        winstonLoader,
+        winstonLoader, //logger
         iocLoader,
-        eventDispatchLoader,
-        typeormLoader,
-        expressLoader,
-        swaggerLoader,
-        monitorLoader,
-        homeLoader,
-        publicLoader
+        // eventDispatchLoader, //events dispatcher
+
+        typeormLoader, //creates typeorm connection to database
+        expressLoader, //creates an instance of express app
+
+        // swaggerLoader, //API Tool to describe and document your api
+        // monitorLoader,
+        // homeLoader,
+
+        publicLoader //loads static files from public folder
         // graphqlLoader,
     ]
 })
