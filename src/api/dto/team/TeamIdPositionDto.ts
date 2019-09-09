4493889cde7c086db679cd557c0f2c4b40c3559c
Leo <lbajdull@gmail.com>
2019-09-09 08:55:33 +0300

    change DTO to use constructor

--- src/api/dto/team/TeamIdPositionDto.ts
+++ src/api/dto/team/TeamIdPositionDto.ts
@@ -1,4 +1,11 @@
+import { UserTeam } from 'src/api/models/UserTeam';
+
 export class TeamIdPositionDto {
+    constructor(userteam: UserTeam) {
+        this.id = userteam.team.id;
+        this.position = userteam.position;
+    }
+
     public id: string;
     public position: string;
 }
